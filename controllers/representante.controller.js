95f988995680ac109ed43ce5335e6bb5ae48eca4
dennisP <dennis.perez@epn.edu.ec>
2025-02-24 12:24:27 -0500

    Se completo las validaciones y se crea el representante con una contraseña hasheada

--- controllers/representante.controller.js
+++ controllers/representante.controller.js
@@ -1,7 +1,8 @@
 // Controlador para el rol de REPRESENTANTE
 
-const Representante = require('.models/representante.model');
-const { validarCedulaEcuatoriana } = require('.utils/validarCedulaEcuatoriana');
+const Representante = require('./models/representante.model');
+const { validarCedulaEcuatoriana } = require('./utils/validarCedulaEcuatoriana');
+const { hashPassword } = require('./utils/hashPassword');
 const path = require('path');
 
 // Create REPRESENTANTE
@@ -21,7 +22,7 @@ const crearRepresentante = async (request, response) => {
         if(!/^\d+$/.test(usuario.nroCedula)) // Comprueba que la cadena solo tenga numeros
             return response.status(400).json({ message: 'Numero de cedula solo debe contener numeros!'});
 
-        if(validarCedulaEcuatoriana(usuario.nroCedula)) // Comprueba si el nroCedula es valido
+        if(!validarCedulaEcuatoriana(usuario.nroCedula)) // Comprueba si el nroCedula es valido
             return response.status(400).json({ message: 'Numero de cedula no valido!'});
     }
     else {
@@ -30,7 +31,7 @@ const crearRepresentante = async (request, response) => {
 
     // Validacion del primer nombre
     if (usuario?.primer_nombre) {
-        if(usuario.primer_nombre.length >= 2 && usuario.primer_nombre.length <= 50 )
+        if(usuario.primer_nombre.length < 2 || usuario.primer_nombre.length > 50 )
             return response.status(400).json({ message: 'El primer nombre debe tener entre 2 y 50 caracteres!'});
         if(!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(usuario.primer_nombre))
             return response.status(400).json({ message: 'El primer nombre solo debe contener letras!'});
@@ -40,9 +41,9 @@ const crearRepresentante = async (request, response) => {
 
     // Validacion del segundo nombre
     if (usuario?.segundo_nombre) {
-        if(usuario.segundo_nombre.length >= 2 && usuario.segundo_nombre.length <= 50 )
+        if(usuario.segundo_nombre.length < 2 || usuario.segundo_nombre.length > 50 )
             return response.status(400).json({ message: 'El segundo nombre debe tener entre 2 y 50 caracteres!'});
-        if(!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(usuario.primer_nombre))
+        if(!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(usuario.segundo_nombre))
             return response.status(400).json({ message: 'El segundo nombre solo debe contener letras!'});
     } else {
         return response.status(400).json({ message: 'El segundo nombre no es valido!'});
@@ -50,9 +51,9 @@ const crearRepresentante = async (request, response) => {
 
     // Validacion primer apellido
     if (usuario?.primer_apellido) {
-        if(usuario.primer_apellido.length >= 2 && usuario.primer_apellido.length <= 50 )
+        if(usuario.primer_apellido.length < 2 || usuario.primer_apellido.length > 50 )
             return response.status(400).json({ message: 'El primer apellido debe tener entre 2 y 50 caracteres!'});
-        if(!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(usuario.primer_nombre))
+        if(!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(usuario.primer_apellido))
             return response.status(400).json({ message: 'El primer apellido solo debe contener letras!'});
     } else {
         return response.status(400).json({ message: 'El primer apellido no es valido!'});
@@ -60,9 +61,9 @@ const crearRepresentante = async (request, response) => {
 
     // Validacion segundo apellido
     if (usuario?.segundo_apellido) {
-        if(usuario.segundo_apellido.length >= 2 && usuario.segundo_apellido.length <= 50 )
+        if(usuario.segundo_apellido.length < 2 || usuario.segundo_apellido.length > 50 )
             return response.status(400).json({ message: 'El segundo apellido debe tener entre 2 y 50 caracteres!'});
-        if(!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(usuario.primer_nombre))
+        if(!/^[A-Za-zÁÉÍÓÚáéíóúÑñ]+$/.test(usuario.segundo_apellido))
             return response.status(400).json({ message: 'El segundo apellido solo debe contener letras!'});
     } else {
         return response.status(400).json({ message: 'El segundo apellido no es valido!'});
@@ -73,7 +74,7 @@ const crearRepresentante = async (request, response) => {
         if(usuario.celular.length < 10)
             return response.status(400).json({ message: 'Numero celular debe tener 10 numeros!'});
 
-        if(!/^\d+$/.test(usuario.nroCedula)) // Comprueba que la cadena solo tenga numeros
+        if(!/^\d+$/.test(usuario.celular)) // Comprueba que la cadena solo tenga numeros
             return response.status(400).json({ message: 'Numero celular solo debe contener numeros!'});
     }
     else {
@@ -90,24 +91,66 @@ const crearRepresentante = async (request, response) => {
     
     // Validar extension archivo cedula PDF 
     if (usuario?.cedula_PDF) {
-        if(path.extname(usuario.cedula_PDF.toLowerCase() === '.pdf')) // Comprueba que la extension sea PDF
-            return response.status(400).json({ message: 'Extension de archivo no valida!'});
+        if(path.extname(usuario.cedula_PDF).toLowerCase() !== '.pdf') // Comprueba que la extension sea PDF
+            return response.status(400).json({ message: 'Extension de archivo cedula PDF no valida!'});
     } else {
-        return response.status(400).json({ message: 'Ruta de archivo no valida!'})
+        return response.status(400).json({ message: 'Ruta de archivo cedula PDF no valida!'})
     }
 
     // Validar extension archivo croquis PDF
+    if (usuario?.croquis_PDF) {
+        if(path.extname(usuario.croquis_PDF).toLowerCase() !== '.pdf') // Comprueba que la extension sea PDF
+            return response.status(400).json({ message: 'Extension de archivo croquis PDF no valida!'});
+    } else {
+        return response.status(400).json({ message: 'Ruta de archivo croquis PDF no valida!'})
+    }
+
+    // Validar telefono convencional
+    if(usuario?.telefono_convencional) { 
+        if(usuario.telefono_convencional.length < 7 || usuario.telefono_convencional.length > 10)
+            return response.status(400).json({ message: 'Numero convencional debe tener entre 7 y 10 numeros!'});
+
+        if(!/^\d+$/.test(usuario.telefono_convencional)) // Comprueba que la cadena solo tenga numeros
+            return response.status(400).json({ message: 'Numero convencional solo debe contener numeros!'});
+    }
+    else {
+        return response.status(400).json({ message: 'Numero convencional no valido!'});
+    }
+
+
+    // Validar telefono de emergencia
+    if(usuario?.telefono_emergencia) { 
+        if(usuario.telefono_emergencia.length < 7 || usuario.telefono_emergencia.length > 10)
+            return response.status(400).json({ message: 'Numero de emergencia debe tener entre 7 y 10 numeros!'});
+
+        if(!/^\d+$/.test(usuario.telefono_emergencia)) // Comprueba que la cadena solo tenga numeros
+            return response.status(400).json({ message: 'Numero de emergencia solo debe contener numeros!'});
+    }
+    else {
+        return response.status(400).json({ message: 'Numero de emergencia no valido!'});
+    }
+
+    // Validar contrasena
+    if(usuario?.contraseña) {
+        if(usuario.contraseña.length < 8 || usuario.contraseña.length > 100)
+            return response.status(400).json({ message: 'La contrasena debe tener al menos 8 caracteres!'});
+    } 
+    else {
+        return response.status(400).json({ message: 'Contrasena no valida!'});
+    }
 
     try {
-        const representanteEncontrado = await Representante.findOne({ 
-            where: { nroCedula: usuario.nroCedula }
-        });
+        const representanteEncontrado = await Representante.findByPk(usuario.nroCedula);
 
         if(representanteEncontrado)
             return response.status(409).json({ message: 'El usuario ya existe!' }); // 409 conflict
 
+        const password = usuario.contraseña;
+        usuario.contraseña = await hashPassword(password);
         const nuevoRepresentante = await Representante.create(usuario);
-        return response.status(200).json({ message: 'Usuario creado exitosamente', nuevoRepresentante});
+        const {contraseña: _, ...result} = nuevoRepresentante.toJSON()
+        
+        return response.status(200).json({ message: 'Usuario creado exitosamente', result});
 
     } catch (error) {
         console.log(error);
