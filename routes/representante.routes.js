aedb1aea722e21a71ef436025793237aac7cc029
dennisP <dennis.perez@epn.edu.ec>
2025-02-26 15:51:59 -0500

    Se añaden las rutas para REPRESENTANTE

---
+++ routes/representante.routes.js
@@ -0,0 +1,9 @@
+const RepresentanteController = require('./controllers/representante.controller');
+
+module.exports = (app) => {
+    app.post('/representante/crear', RepresentanteController.crearRepresentante);
+    app.put('/representante/editar/:cedula', RepresentanteController.updateRepresentante);
+    app.get('/representante/obtener/:cedula', RepresentanteController.getRepresentante);
+    app.get('/representante/obtener', RepresentanteController.getAllRepresentantes);
+    app.delete('/representante/eliminar/:cedula', RepresentanteController.deleteRepresentante);
+}
