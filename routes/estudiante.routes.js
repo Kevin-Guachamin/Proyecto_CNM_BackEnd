bbea59aeb7b75adc3382a8655f8578a39251181e
dennisP <dennis.perez@epn.edu.ec>
2025-04-01 16:10:05 -0500

    Se comenta el endpoint /estudiante/obtener/:ID

--- routes/estudiante.routes.js
+++ routes/estudiante.routes.js
@@ -5,7 +5,7 @@ const EstudianteController = require('../controllers/estudiante.controller');
 module.exports = (app) => {
     app.post('/estudiante/crear', upload.uploadEstudiantesFields,EstudianteController.crearEstudiante);
     app.put('/estudiante/editar/:ID',upload.uploadEstudiantesFields, EstudianteController.updateEstudiante);
-    app.get('/estudiante/obtener/:ID', EstudianteController.getEstudiante);
+    //app.get('/estudiante/obtener/:ID', EstudianteController.getEstudiante);
     app.get('/estudiante/obtener', EstudianteController.getAllEstudiantes);
     app.delete('/estudiante/eliminar/:ID', EstudianteController.deleteEstudiante);
     app.get('/api/representantes/:cedula/estudiantes', EstudianteController.getRepresentanteEstudiante);
