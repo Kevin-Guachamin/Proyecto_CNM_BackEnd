4daed7e737c495e92f75edfb8ecc78df85a60f1a
Bryan <bryanlug2014@gmail.com>
2025-06-02 20:03:49 -0500

    corrección momentanea para crear admin desde postman

--- routes/docente.routes.js
+++ routes/docente.routes.js
@@ -1,12 +1,10 @@
 const DocenteController = require('../controllers/docente.controller')
-const { changePassword } = require('../controllers/password.controller')
-const { validatePasswordChange } = require('../controllers/password.controller')
 const { Docente, docenteAdministrador } = require('../middlewares/protect')
 
 
 
 module.exports = (app) => {
-    app.post('/api/docente/crear', docenteAdministrador, DocenteController.createDocente)
+    app.post('/api/docente/crear', DocenteController.createDocente)
     app.put('/api/docente/editar/:cedula', docenteAdministrador, DocenteController.editDocente)
     app.get('/api/docente/obtener/:cedula', DocenteController.getDocente)
     app.get('/api/docente/obtener', Docente, DocenteController.getDocentes)
