a24ac14eca77f9f61bfe00b9ee67880d37742382
Bryan <bryanlug2014@gmail.com>
2025-06-06 21:31:07 -0500

    ajuste para ver las tablas antes de el app listen

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const sequelize=require('./config/sequelize.config');
-const {reprogramarPeriodosPendientes}=require('./controllers/programarCierre.controller')
+const sequelize = require('./config/sequelize.config');
+const { reprogramarPeriodosPendientes } = require('./controllers/programarCierre.controller')
 
 require('./models/docente.model')
 require('./models/materia.model')
@@ -12,11 +12,11 @@ require('./models/fechas_notas.model')
 
 
 const express = require('express')
-const cors= require('cors')
+const cors = require('cors')
 
-const app= express();
+const app = express();
 app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // NO RECOMENDADO PARA PRODUCCION
 app.use(cors({
@@ -24,63 +24,65 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
-const port=8000;
+const port = 8000;
 const startServer = async () => {
     try {
         const mensaje = await sequelize.conexion(); // Espera a que la BD se sincronice
         console.log(mensaje);
 
         // Una vez sincronizada, inicia el servidor
-        app.listen(port, "0.0.0.0",() => {
+        const [results] = await sequelize.query("SHOW TABLES");
+        console.log("Tablas disponibles:", results);
+        app.listen(port, "0.0.0.0", () => {
             console.log("Server listening at port", port);
         });
     } catch (error) {
         console.error("Error al sincronizar la base de datos:", error);
     }
 };
-const allDocente= require('./routes/docente.routes')
+const allDocente = require('./routes/docente.routes')
 allDocente(app)
 
-const allCalificaciones= require('./routes/calificaciones.routes')
+const allCalificaciones = require('./routes/calificaciones.routes')
 allCalificaciones(app)
 
-const allRepresentante= require('./routes/representante.routes')
+const allRepresentante = require('./routes/representante.routes')
 allRepresentante(app)
 
-const allEstudiante= require('./routes/estudiante.routes')
+const allEstudiante = require('./routes/estudiante.routes')
 allEstudiante(app)
 
 const allMateria = require('./routes/materia.routes')
 allMateria(app)
 
-const allPeriodos= require('./routes/periodo_academico.routes')
+const allPeriodos = require('./routes/periodo_academico.routes')
 allPeriodos(app)
 // Llamar a la función para iniciar
-const allEstudiantes=require('./routes/estudiante.routes')
+const allEstudiantes = require('./routes/estudiante.routes')
 allEstudiantes(app)
 
-const allAsignacion=require('./routes/asignacion.routes')
+const allAsignacion = require('./routes/asignacion.routes')
 allAsignacion(app)
 
-const allLogin=require('./routes/login.routes')
+const allLogin = require('./routes/login.routes')
 allLogin(app)
 
-const allInscripcion=require('./routes/inscripcion.routes')
+const allInscripcion = require('./routes/inscripcion.routes')
 allInscripcion(app)
 
-const AllFechasNotas=require('./routes/fechas.routes')
+const AllFechasNotas = require('./routes/fechas.routes')
 AllFechasNotas(app)
 
-const AllMatriculas= require('./routes/matricula.route')
+const AllMatriculas = require('./routes/matricula.route')
 AllMatriculas(app)
 
-const AllSolicitudes=require('./routes/solicitud.routes')
+const AllSolicitudes = require('./routes/solicitud.routes')
 AllSolicitudes(app)
 
-const AllPassword=require('./routes/password.routes')
+const AllPassword = require('./routes/password.routes')
 AllPassword(app)
 
-const AllFiles=require('./routes/downlodadFile.routes')
+const AllFiles = require('./routes/downlodadFile.routes')
 AllFiles(app)
 
 reprogramarPeriodosPendientes()
